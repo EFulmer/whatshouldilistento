7a2bb7408889204de7aa5a11fe9b7f183eb2b855
Eric Fulmer <EFulmer@gmail.com>
2014-06-04 20:52:46 -0400

    updated xpath to return a (title, rating) tuple list

--- app/get_albums.py
+++ app/get_albums.py
@@ -6,6 +6,7 @@ from lxml import html
 
 
 RYM_BASE = 'http://rateyourmusic.com/artist/'
+ROOT_ALBUM_XPATH = '//div[@id="disco_type_s"]'
 
 def rymify_name(name):
     """
@@ -21,6 +22,9 @@ def get_artist_page(artist_name):
     Return a requests.models.Response representing RYM's page for 
     artist_name.
     """
+    # in the long term future I'd like to handle artist names more 
+    # intelligently, i.e. distinguish between "Kurt Vile" and 
+    # "Kurt Vile & The Violators"
     return requests.get( RYM_BASE + rymify_name(artist_name) )
 
 
@@ -30,15 +34,21 @@ def tree_from_page(artist_page):
     """
     return html.fromstring(artist_page.text)
 
-# //div[@class="disco_release"] -- this returns a list of 
-# lxml.HtmlElements, for parsing further.
-
 
 def get_albums(html_tree):
     """
     Return each album from the artist, given an lxml HtmlTree.
     """
-    return html_tree.xpath('//div[@class="disco_release"]')
+    # '//div[@id="disco_type_s"]//div[@class="disco_mainline"]'
+    # '//div[@id="disco_type_s"]//div[@class="disco_avg_rating"]/text'
+    # return html_tree.xpath('//div[@id="disco_type_s"]')
+    title_xpath = ROOT_ALBUM_XPATH + '//a[@class="album"]/text()'
+    rating_xpath = ROOT_ALBUM_XPATH + '//div[@class="disco_avg_rating"]/text()'
+    titles = html_tree.xpath(title_xpath)
+    ratings = html_tree.xpath(rating_xpath)
+    # return html_tree.xpath('//div[@id="disco_type_s"]//a[@class="album"]/text()')
+    return zip(titles, ratings)
+
 
 
 def get_best_album(albums):
