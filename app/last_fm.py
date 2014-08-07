2b57a34bdaf0353bda4bd0da1bcf678958c79986
Eric Fulmer <EFulmer@gmail.com>
2014-08-06 21:04:24 -0400

    added code to call last.fm API and get top album from artist, replacing RYM

---
+++ app/last_fm.py
@@ -0,0 +1,26 @@
+from collections import namedtuple
+import json
+
+import requests 
+
+
+AS_API_ROOT = 'http://ws.audioscrobbler.com/2.0/'
+AS_API_KEY  = '8c5b0975a02e30db77066c474a9c0333'
+
+ArtistInfo = namedtuple('ArtistInfo', ['artist', 'album'])
+
+
+def get_best_album(artist):
+    s = requests.Session()
+    s.params = { 'api_key' : AS_API_KEY, 
+                 'format' : 'json', 
+                 'artist' : artist,
+                 'method' : 'artist.gettopalbums',
+                 'limit'  : '1'
+                 }
+    r = s.get(AS_API_ROOT)
+    j = json.loads(r.text)
+    name = j[u'topalbums'][u'album'][u'name']
+    album = j[u'topalbums'][u'album'][u'artist'][u'name']
+    return ArtistInfo(name, album)
+
